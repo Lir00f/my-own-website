99c463abdb98d68fc3fff36818d4c5585c310c54
Lir00f <nikolaenko.e.i.21@gmail.com>
2022-09-16 01:06:46 +0300

    final for 16.09

--- js/app.js
+++ js/app.js
@@ -25,3 +25,19 @@ window.onscroll = function() {
 }
 
 window.scrollTo(0, 1)
+
+let soundButton = document.querySelector('.soundbutton'),
+		audio = document.querySelector('.audio')
+
+soundButton.addEventListener('click', e => {
+	soundButton.classList.toggle('paused')
+	audio.paused ? audio.play() : audio.pause()
+})
+
+window.onfocus = function() {
+	soundButton.classList.contains('paused') ? audio.pause() : audio.play()
+}
+
+window.onblur = function() {
+	audio.pause()
+}
